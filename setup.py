e2864e7ed54538533b8b42e60e2f6b4db8127af3
Robbie McDowall <robbie.mcdowall@gmail.com>
2020-01-19 12:53:52 +1000

    Robbie McDowall - increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='opennempy',
-   version='0.3',
+   version='0.4',
    description='Downloads data from http://opennem.org.au',
    author='See https://github.com/opennem/opennempy',
    author_email='See https://github.com/opennem/opennempy',
