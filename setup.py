f7e78bbcdf5bbdcabce00cc99bf7d80f995912fe
Robbie McDowall <robbie.mcdowall@gmail.com>
2020-01-19 11:40:12 +1000

    Robbie McDowall - defining python package To make it easier to install

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='opennempy',
+   version='0.1',
+   description='Downloads data from http://opennem.org.au',
+   author='See https://github.com/opennem/opennempy',
+   author_email='See https://github.com/opennem/opennempy',
+   packages=['opennempy'],
+   install_requires=[
+       'requests',
+       'simplejson',
+       'pandas'
+   ]
+)
