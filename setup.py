8193316d3467c5d416717bcbdc284d0ae7e0b885
Robbie McDowall <robbie.mcdowall@gmail.com>
2020-01-19 11:51:14 +1000

    Robbie McDowall - put config under source control so that it works out of the box

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='opennempy',
-   version='0.1',
+   version='0.2',
    description='Downloads data from http://opennem.org.au',
    author='See https://github.com/opennem/opennempy',
    author_email='See https://github.com/opennem/opennempy',
